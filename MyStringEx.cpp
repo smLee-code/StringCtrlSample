ffd667236f2d0f8210468354847e9ac3e20bd92c
leesm4510@gmail.com <leesm4510@gmail.com>
2022-05-18 18:57:25 +0900

    실습 과제 10 - 풀이 수정 및 정답

--- MyStringEx.cpp
+++ MyStringEx.cpp
@@ -1,13 +1,6 @@
 #include "pch.h"
 #include "MyStringEx.h"
 
-void CMyStringEx::SetString(const char* pszParam)
-{
-	SetString(pszParam);
-
-	if (strstr(GetString(), "멍멍이아들") != NULL)
-		SetString("착한 사람");
-}
 
 int CMyStringEx::Find(const char* pszParam)
 {
@@ -21,4 +14,27 @@ int CMyStringEx::Find(const char* pszParam)
 	
 	return -1;
 
+}
+
+int CMyStringEx::SetString(const char* pszParam)
+{
+
+	/* 답안 코드 : 필터링 문자열과 정확히 일치하지 않으면 안된다.
+	int nResultLen;
+
+	if (strcmp(pszParam, "멍멍이아들") == 0)
+		nResultLen = CMyString::SetString("착한 사람");
+	else
+		nResultLen = CMyString::SetString(pszParam);
+
+	return nResultLen;
+	*/
+
+	// 풀이 코드 : 필터링 문자열이 포함만 되있어도 필터링 한다.
+	int nResultLen = SetString(pszParam);
+
+	if (strstr(GetString(), "멍멍이아들") != NULL)
+		return CMyString::SetString("착한 사람");
+
+	return nResultLen;
 }
--- MyStringEx.h
+++ MyStringEx.h
@@ -5,7 +5,7 @@ class CMyStringEx : public CMyString
 private:
 
 public:
-	void SetString(const char* pszParam);
 	int Find(const char* pszParam);
+	int SetString(const char* pszParam);
 };
 
